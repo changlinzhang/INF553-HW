89135b6b08684b5be0b13e4382c7a47da83e3615
changlinzhang <598107049@qq.com>
2019-04-19 13:56:51 -0700

    added centroid version

--- cure/cure_representatives.py
+++ cure/cure_representatives.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import heapq
+# from Queue import PriorityQueue
 
 
 class Data:
@@ -29,7 +30,11 @@ class Cluster:
         for i in range(Data.dim):
             f = p.features[i]
             pp.features = np.append(pp.features, f + alpha * (self.centroid.features[i] - f))
-        pp.i = p.i
+        # print("*******************")
+        # print(p.features)
+        # print(self.centroid.features)
+        # print(pp.features)
+        # print("*******************")
         return pp
 
 
@@ -48,15 +53,12 @@ def merge(u, v):
     for i in range(Data.dim):
         f_i = (u.centroid.features[i] * u_num + v.centroid.features[i] * v_num) / (u_num + v_num)
         w.centroid.features = np.append(w.centroid.features, f_i)
-    return w
 
-
-def find_repre(w):
     if len(w.datas) <= Cluster.n:
         for p in w.datas:
             pp = w.shrink2centroid(p)
             w.reps = np.append(w.reps, pp)
-        return
+        return w
 
     #else
     tmpSet = set()
@@ -78,7 +80,7 @@ def find_repre(w):
         pp = w.shrink2centroid(p)
         w.reps = np.append(w.reps, pp)
 
-    return
+    return w
 
 
 def cluster(samples, k):
@@ -102,7 +104,9 @@ def cluster(samples, k):
         clusters_set.remove(u)
         clusters_set.remove(v)
         for c in clusters_set:
-            d = cluster_dist(w, c)
+            d, min_pair = cluster_dist(w, c)
+            if len(w.datas) == 3 and len(c.datas) == 3:
+                print(min_pair)
             heapq.heappush(pairs, [d, w, c])
         clusters_set.add(w)
     return clusters_set
@@ -123,8 +127,15 @@ def initial2clusters(samples):
 
 
 def cluster_dist(c1, c2):
-    dist = np.sum(np.square(c1.centroid.features - c2.centroid.features))
-    return dist
+    min_d = sys.float_info.max
+    min_pair = None
+    for p1 in c1.reps:
+        for p2 in c2.reps:
+            d = data_dist(p1, p2)
+            if d < min_d:
+                min_d = d
+                min_pair = (p1, p2)
+    return min_d, min_pair
 
 
 def data2cluster_dist(data, c):
@@ -140,7 +151,7 @@ def pairwise_distance(clusters):
     pairs = []
     for i in range(len(clusters)):
         for j in range(i+1, len(clusters)):
-            dist = cluster_dist(clusters[i], clusters[j])
+            dist, _ = cluster_dist(clusters[i], clusters[j])
             pairs.append([dist, clusters[i], clusters[j]])
     return pairs
 
@@ -149,6 +160,10 @@ def read_sample(sample_file):
     file = open(sample_file, 'r')
     content = file.readlines()
     samples_indexs = [int(line) for line in content]
+    # content = file.readlines()[0]
+    # content = content[1:len(content)-1]
+    # samples_indexs = content.split(',')
+    # samples_indexs = [int(s) for s in samples_indexs]
     file.close()
     return samples_indexs
 
@@ -159,7 +174,7 @@ def read_dataset(dataset_file):
     datas = []
     for i in range(len(content)):
         features = content[i].split(',')
-        label = features[-1].strip()
+        label = features[-1]
         features = features[:-1]
         features = [float(f) for f in features]
         data = Data(i, np.array(features), label)
@@ -179,14 +194,12 @@ def read_dataset(dataset_file):
 #     file.close()
 
 
-def sort_cluster(clusters_list):
-    # sort inner reps
-    for c in clusters_list:
-        c.reps = sorted(c.reps, key=lambda rep: rep.i)
-        c.datas = sorted(c.datas, key=lambda data: data.i)
-    # sort clusters
-    clusters_list = sorted(clusters_list, key=lambda c: c.reps[0].i)
-    return clusters_list
+# def sort_cluster(clusters_list):
+#     # sort inner reps
+#     for c in clusters_list:
+#         c.reps.sort(key=lambda rep: rep.i)
+#     # sort clusters
+#     clusters_list.sort(key=lambda c: c.reps[0].i)
 
 
 def label_samples(clusters_list):
@@ -198,8 +211,6 @@ def label_samples(clusters_list):
 def predict(clusters_list, datas):
     for i in range(len(datas)):
         data = datas[i]
-        if data.predict >= 0:
-            continue
         if not data.predict:
             min_dist = sys.float_info.max
             predict = -1
@@ -223,28 +234,9 @@ def sample(samples_indexs, datas):
 def eval(clusters_list, datas):
     truth_pairs = []
     predict_pairs = []
-
-    for c in clusters_list:
-        datas_tmp = c.datas
-        for i in range(len(datas_tmp)):
-            for j in range(i+1, len(datas_tmp)):
-                predict_pairs.append((datas_tmp[i].i, datas_tmp[j].i))
-
-    true_cluster = {}
-    for d in datas:
-        if d.label not in true_cluster:
-            true_cluster[d.label] = []
-        true_cluster[d.label].append(d.i)
-
-    for value in true_cluster.values():
-        datas = sorted(value)
-        for i in range(len(datas)):
-            for j in range(i+1, len(datas)):
-                truth_pairs.append((datas[i], datas[j]))
-
     intersection = set(truth_pairs) & set(predict_pairs)
     precision = 1.0*len(intersection)/len(predict_pairs)
-    recall = 1.0*len(intersection)/len(truth_pairs)
+    recall = .0*len(intersection)/len(truth_pairs)
     return precision, recall
 
 
@@ -260,27 +252,24 @@ if __name__ == "__main__":
 
     samples_indexs = read_sample(sample_file)
     datas = read_dataset(dataset_file)
-    ori_datas = datas
     samples = sample(samples_indexs, datas)
 
-    #samples clustering results
+    # gene_test_file(dataset_file, samples_indexs)
+
+    # output samples clustering results
+    file = open('output5.txt', 'w')
     clusters_set = cluster(samples, k)
-    for w in clusters_set:
-        find_repre(w)
     clusters_list = (list(clusters_set))
-    clusters_list = sort_cluster(clusters_list)
-    label_samples(clusters_list)
-
-    predict(clusters_list, datas)
-    for i in range(len(clusters_list)):
-        pre_s = 'Cluster ' + str(i) + ':'
-        c = clusters_list[i]
+    # sort_cluster(clusters_list)
+    # label_samples(clusters_list)
+    print(len(clusters_set))
+    for c in clusters_set:
         indexs = []
-        datas_tmp = c.datas
-        for d in datas_tmp:
+        for d in c.datas:
             indexs.append(d.i)
-        print(pre_s + str(indexs))
+        file.write(str(indexs)+'\n')
+    file.close()
 
-    precision, recall = eval(clusters_list, ori_datas)
-    print('Precision = %.12f, recall = %0.12f' % (precision, recall))
+    # predict(clusters_list, datas)
+    # eval(clusters_list, datas)
 
